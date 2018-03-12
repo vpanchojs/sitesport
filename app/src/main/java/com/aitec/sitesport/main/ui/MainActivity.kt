caf162313530b9bd00c71c40db5249c25e152fde
Victor Francisco Jumbo Sinchire <vpanchojs@gmail.com>
2018-03-12 15:57:59 -0500

    Button profile in futbol

--- app/src/main/java/com/aitec/sitesport/main/ui/MainActivity.kt
+++ app/src/main/java/com/aitec/sitesport/main/ui/MainActivity.kt
@@ -1,18 +1,30 @@
 package com.aitec.sitesport.main.ui
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.app.Activity
 import android.content.Intent
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.location.Location
 import android.os.Bundle
+import android.os.Looper
 import android.support.design.widget.BottomSheetBehavior
+import android.support.v4.app.ActivityCompat
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.SearchView
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import com.aitec.sitesport.R
 import com.aitec.sitesport.entities.Entrepise
 import com.aitec.sitesport.main.adapter.EntrepiseAdapter
 import com.aitec.sitesport.profile.ProfileActivity
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
 import com.mapbox.mapboxsdk.annotations.IconFactory
 import com.mapbox.mapboxsdk.annotations.MarkerOptions
 import com.mapbox.mapboxsdk.camera.CameraPosition
@@ -24,21 +36,153 @@ import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.bottom_sheet_results.*
 
 
-class MainActivity : AppCompatActivity(), EntrepiseAdapter.onEntrepiseAdapterListener, SelectDistanceFragment.OnSelectDistanceListener, SearchView.OnQueryTextListener, View.OnFocusChangeListener, OnMapReadyCallback, View.OnClickListener {
+class MainActivity : AppCompatActivity(), EntrepiseAdapter.onEntrepiseAdapterListener, SelectDistanceFragment.OnSelectDistanceListener, SearchView.OnQueryTextListener, View.OnFocusChangeListener, OnMapReadyCallback, View.OnClickListener, MapboxMap.OnCameraIdleListener {
 
+    private val TAG = MainActivity::class.java.simpleName
+    /**
+     * Code used in requesting runtime permissions.
+     */
+    private val REQUEST_PERMISSIONS_REQUEST_CODE = 34
+    /**
+     * Constant used in the location settings dialog.
+     */
+    private val REQUEST_CHECK_SETTINGS = 0x1
+    /**
+     * The desired interval for location updates. Inexact. Updates may be more or less frequent.
+     */
+    private val UPDATE_INTERVAL_IN_MILLISECONDS: Long = 10000
+    /**
+     * The fastest rate for active location updates. Exact. Updates will never be more frequent
+     * than this value.
+     */
+    private val FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS = UPDATE_INTERVAL_IN_MILLISECONDS / 2
+    // Keys for storing activity state in the Bundle.
+    private val KEY_REQUESTING_LOCATION_UPDATES = "requesting-location-updates"
+    private val KEY_LOCATION = "location"
+    private val KEY_LAST_UPDATED_TIME_STRING = "last-updated-time-string"
+
+
+    val REQUESTING_LOCATION_UPDATES_KEY = "location"
+    var requestingLocationUpdates = false
     lateinit var entrepiseAdapter: EntrepiseAdapter
     var data = ArrayList<Entrepise>()
+    lateinit var mapboxMap: MapboxMap
+    lateinit var iconFactory: IconFactory
 
+    private lateinit var fusedLocationClient: FusedLocationProviderClient
+    private lateinit var locationRequest: LocationRequest
+    private lateinit var locationCallback: LocationCallback
+    private lateinit var mSettingsClient: SettingsClient
+    private lateinit var mCurrentLocation: Location
+    private lateinit var mLocationSettingsRequest: LocationSettingsRequest
+
+
+    @SuppressLint("MissingPermission")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        //Mapbox.getInstance(this, getString(R.string.accessTokenMapBox));
         setContentView(R.layout.activity_main)
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
+        mSettingsClient = LocationServices.getSettingsClient(this)
+        createLocationCallback()
+        createLocationRequest()
+        buildLocationSettingsRequest()
         setupMap(savedInstanceState)
         setupBottomSheet()
         setupReciclerView()
         setupEvents()
+
     }
 
+    private fun buildLocationSettingsRequest() {
+        mLocationSettingsRequest = LocationSettingsRequest.Builder()
+                .addLocationRequest(locationRequest).build()
+    }
+
+    private fun createLocationCallback() {
+        locationCallback = object : LocationCallback() {
+            override fun onLocationResult(locationResult: LocationResult?) {
+                locationResult ?: return
+                for (location in locationResult.locations) {
+                    Log.e(TAG, "" + location.latitude)
+                    mCurrentLocation = location
+                }
+            }
+        }
+    }
+
+    fun createLocationRequest() {
+        locationRequest = LocationRequest().apply {
+            interval = UPDATE_INTERVAL_IN_MILLISECONDS
+            fastestInterval = FASTEST_UPDATE_INTERVAL_IN_MILLISECONDS
+            priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        }
+
+/*
+        val client: SettingsClient = LocationServices.getSettingsClient(this)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+        task.addOnSuccessListener { locationSettingsResponse ->
+            Log.e("aa", locationSettingsResponse.toString())
+        }
+
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException) {
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+
+                    exception.startResolutionForResult(this@MainActivity,
+                            REQUEST_CHECK_SETTINGS)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+        */
+    }
+
+
+    @SuppressLint("MissingPermission")
+    private fun startLocationUpdates() {
+        //fusedLocationClient.requestLocationUpdates(locationRequest, locationCallback, null)
+        mSettingsClient.checkLocationSettings(mLocationSettingsRequest)
+                .addOnSuccessListener(this) {
+                    Log.e(TAG, "All location settings are satisfied.")
+                    fusedLocationClient.requestLocationUpdates(locationRequest,
+                            locationCallback, Looper.myLooper())
+
+                }
+                .addOnFailureListener(this) { e ->
+                    val statusCode = (e as ApiException).statusCode
+                    when (statusCode) {
+                        LocationSettingsStatusCodes.RESOLUTION_REQUIRED -> {
+                            Log.e(TAG, "Location settings are not satisfied. Attempting to upgrade " + "location settings ")
+                            try {
+                                // Show the dialog by calling startResolutionForResult(), and check the
+                                // result in onActivityResult().
+                                val rae = e as ResolvableApiException
+                                rae.startResolutionForResult(this@MainActivity, REQUEST_CHECK_SETTINGS)
+                            } catch (sie: IntentSender.SendIntentException) {
+                                //   Log.i(FragmentActivity.TAG, "PendingIntent unable to execute request.")
+                            }
+
+                        }
+                        LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE -> {
+                            val errorMessage = "Location settings are inadequate, and cannot be " + "fixed here. Fix in Settings."
+                            //   Log.e(FragmentActivity.TAG, errorMessage)
+                            Toast.makeText(this@MainActivity, errorMessage, Toast.LENGTH_LONG).show()
+                            // requestingLocationUpdates = false
+                        }
+                    }
+                    // updateUI()
+                }
+    }
+
+    private fun stopLocationUpdates() {
+        fusedLocationClient.removeLocationUpdates(locationCallback)
+    }
 
     fun setupReciclerView() {
         data.add(Entrepise("Sitio Deportivo"))
@@ -88,6 +232,34 @@ class MainActivity : AppCompatActivity(), EntrepiseAdapter.onEntrepiseAdapterLis
     public override fun onResume() {
         super.onResume()
         mapView.onResume()
+        if (checkPermissions()) {
+            startLocationUpdates();
+        } else if (!checkPermissions()) {
+            requestPermissions();
+        }
+    }
+
+    private fun requestPermissions() {
+        val shouldProvideRationale = ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+
+        if (shouldProvideRationale) {
+
+            ActivityCompat.requestPermissions(this@MainActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_PERMISSIONS_REQUEST_CODE)
+        } else {
+
+            ActivityCompat.requestPermissions(this@MainActivity,
+                    arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                    REQUEST_PERMISSIONS_REQUEST_CODE)
+        }
+    }
+
+    private fun checkPermissions(): Boolean {
+        var permissionState = ActivityCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION);
+        return permissionState == PackageManager.PERMISSION_GRANTED;
     }
 
     override fun onStart() {
@@ -103,6 +275,7 @@ class MainActivity : AppCompatActivity(), EntrepiseAdapter.onEntrepiseAdapterLis
     public override fun onPause() {
         super.onPause()
         mapView.onPause()
+        stopLocationUpdates()
     }
 
     override fun onLowMemory() {
@@ -116,14 +289,69 @@ class MainActivity : AppCompatActivity(), EntrepiseAdapter.onEntrepiseAdapterLis
     }
 
     override fun onSaveInstanceState(outState: Bundle?) {
+        outState?.putBoolean(REQUESTING_LOCATION_UPDATES_KEY, requestingLocationUpdates)
         super.onSaveInstanceState(outState)
         mapView.onSaveInstanceState(outState!!)
+    }
+
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        when (requestCode) {
+        // Check for the integer request code originally supplied to startResolutionForResult().
+            REQUEST_CHECK_SETTINGS -> {
+                when (resultCode) {
+                    Activity.RESULT_OK -> {
+                        Log.i(TAG, "User agreed to make required location settings changes.");
+// Nothing to do. startLocationupdates() gets called in onResume again.
+                    }
+                    Activity.RESULT_CANCELED -> {
+                        Log.i(TAG, "User chose not to make required location settings changes.");
+                        //requestingLocationUpdates = false;
+                        //updateUI();
+                    }
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        Log.i(TAG, "onRequestPermissionResult");
+        if (requestCode == REQUEST_PERMISSIONS_REQUEST_CODE) {
+            if (grantResults.size <= 0) {
+
+                Log.i(TAG, "User interaction was cancelled.");
+            } else if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // if (requestingLocationUpdates) {
+                Log.i(TAG, "Permission granted, updates requested, starting location updates");
+                startLocationUpdates();
+                //}
+            } else {
+                /*
+                showSnackbar(R.string.permission_denied_explanation,
+                        R.string.settings, new View . OnClickListener () {
+                    @Override
+                    public void onClick(View view) {
+                        // Build intent that displays the App settings screen.
+                        Intent intent = new Intent();
+                        intent.setAction(
+                                Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+                        Uri uri = Uri . fromParts ("package",
+                        BuildConfig.APPLICATION_ID, null);
+                        intent.setData(uri);
+                        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(intent);
+                    }
+                })
+                */
+            }
+        }
     }
 
     override fun onRecoveryPassword(email: String) {
 
     }
-
 
     override fun onClick(p0: View?) {
         when (p0!!.id) {
@@ -137,10 +365,14 @@ class MainActivity : AppCompatActivity(), EntrepiseAdapter.onEntrepiseAdapterLis
         }
     }
 
+    override fun onMapReady(mapboxMaps: MapboxMap?) {
+        this.mapboxMap = mapboxMaps!!
+        iconFactory = IconFactory.getInstance(this)
+        addMarker()
+        mapboxMap.addOnCameraIdleListener(this)
+    }
 
-    override fun onMapReady(mapboxMap: MapboxMap?) {
-        Log.e("HOLA", "mapa")
-        val iconFactory = IconFactory.getInstance(this)
+    fun addMarker() {
         var icon = iconFactory.fromResource(R.drawable.ic_ball_futbol)
         mapboxMap!!.addMarker(MarkerOptions()
                 .position(LatLng(-4.028872, -79.213712))
@@ -153,6 +385,12 @@ class MainActivity : AppCompatActivity(), EntrepiseAdapter.onEntrepiseAdapterLis
         mapboxMap.animateCamera(CameraUpdateFactory.newCameraPosition(position), 2000)
     }
 
+    override fun onCameraIdle() {
+        val cameraPosition = mapboxMap.cameraPosition
+        val lat = cameraPosition.target.latitude
+        val lng = cameraPosition.target.longitude
+        Log.e("coordenadas", "" + lat + " " + lng)
+    }
 
     override fun onFocusChange(v: View?, hasFocus: Boolean) {
         if (hasFocus) {
