dd2d69e3476e7933592950c85ef0b85d27a01475
Victor Francisco Jumbo Sinchire <vpanchojs@gmail.com>
2018-03-15 18:54:58 -0500

    Implementacio de Retrofit

--- app/src/main/java/com/aitec/sitesport/entities/Entrepise.kt
+++ app/src/main/java/com/aitec/sitesport/entities/Entrepise.kt
@@ -1,5 +1,15 @@
 package com.aitec.sitesport.entities
 
-class Entrepise(var name: String) {
+class Entrepise {
+    lateinit var name: String
+    lateinit var phones: Array<String>
+    lateinit var category: String
+    //lateinit var horarios: HashMap<String, String>
+    lateinit var visites: String
+    lateinit var number_field: String
+    lateinit var photos: Array<String>
+    var open = false
+    var distance: Double = 0.0
+
 
 }
