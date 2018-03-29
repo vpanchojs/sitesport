9387d4f45418444e5155e97a6db79eec0e620cd8
Yavac <jl.sys.win@gmail.com>
2018-03-28 19:01:46 -0500

    IU Profile

--- app/src/main/java/com/aitec/sitesport/profile/ProfilePresenterImpl.kt
+++ app/src/main/java/com/aitec/sitesport/profile/ProfilePresenterImpl.kt
@@ -38,8 +38,8 @@ class ProfilePresenterImpl(var profileView : ProfileView,
                 profileView.setNameProfile((profileEvent.eventEnterprise as Enterprise).nombres)
                 profileView.setImageProfile((profileEvent.eventEnterprise as Enterprise).fotos)
                 profileView.setStateEnterprise(if((profileEvent.eventEnterprise as Enterprise).abierto) "abierto" else "cerrado")
-                profileView.setPriceDayStandar((profileEvent.eventEnterprise as Enterprise).precio!![0].valor)
-                profileView.setPriceNightStandar((profileEvent.eventEnterprise as Enterprise).precio!![1].valor)
+                profileView.setPriceDayStandar((profileEvent.eventEnterprise as Enterprise).precio!![0].dia)
+                profileView.setPriceNightStandar((profileEvent.eventEnterprise as Enterprise).precio!![1].noche)
             }
 
             ProfileEvent.ERROR_PROFILE -> {
