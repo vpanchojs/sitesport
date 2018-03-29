7c1db59b8414909971ed8cbd73225c7e1f7ea9e5
Yavac <jl.sys.win@gmail.com>
2018-03-29 18:34:35 -0500

    IU Profile

--- app/src/main/java/com/aitec/sitesport/profile/ui/ProfileActivity.kt
+++ app/src/main/java/com/aitec/sitesport/profile/ui/ProfileActivity.kt
@@ -120,22 +120,10 @@ class ProfileActivity : AppCompatActivity(), AppBarLayout.OnOffsetChangedListene
     }
 
     override fun showContentLoading() {
-        //val params = toolbar_profile.getLayoutParams() as AppBarLayout.LayoutParams
-        //params.scrollFlags = 0  // clear all scroll flags
-        nsvProfile.setNestedScrollingEnabled(false)
-        toolbar_profile.title = enterprise!!.nombres
-        //app_bar_layout_profile.setExpanded(false)
         contentLoading.visibility = View.VISIBLE
     }
 
     override fun hideContentLoading() {
-        //val params = toolbar_profile.getLayoutParams() as AppBarLayout.LayoutParams
-        //params.scrollFlags = 1  // clear all scroll flags
-        toolbar_profile.title = ""
-        nsvProfile.setNestedScrollingEnabled(true)
-        collapse_toolbar_profile.title = ""
-        header_tv_title.text = enterprise!!.nombres
-        //app_bar_layout_profile.setExpanded(true)
         contentLoading.visibility = View.GONE
     }
 
