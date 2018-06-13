2b22a836ac1c954eedca45a66cb25966bc310bb5
Yavac <123bitbucket123@>
2018-06-13 15:14:02 -0500

    Change model

---
+++ app/src/main/java/com/aitec/sitesport/entities/enterprise/Servicio.kt
@@ -0,0 +1,36 @@
+package com.aitec.sitesport.entities.enterprise
+
+import android.os.Parcel
+import android.os.Parcelable
+
+class Servicio() : Parcelable {
+
+    var nombre: String = ""
+    var descripcion: String = ""
+
+    constructor(parcel: Parcel) : this() {
+        nombre = parcel.readString()
+        descripcion = parcel.readString()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(nombre)
+        parcel.writeString(descripcion)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Servicio> {
+        override fun createFromParcel(parcel: Parcel): Servicio {
+            return Servicio(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Servicio?> {
+            return arrayOfNulls(size)
+        }
+    }
+
+
+}
