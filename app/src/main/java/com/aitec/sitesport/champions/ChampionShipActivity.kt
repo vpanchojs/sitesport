c9127cca795f11ddf267d8386b0118eda6b2face
Victor Francisco Jumbo Sinchire <vpanchojs@gmail.com>
2018-07-02 18:31:11 -0500

    Interfaz 1.0

--- app/src/main/java/com/aitec/sitesport/champions/ChampionShipActivity.kt
+++ app/src/main/java/com/aitec/sitesport/champions/ChampionShipActivity.kt
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentPagerAdapter
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.view.*
 import com.aitec.sitesport.R
